b64c7dc88479cb3cea6671be8430d7aaa3010bbf
Jehan Veracierta <jehanzao@gmail.com>
2022-09-12 00:50:16 -0400

    Toda la visibilidad lista

--- src/components/layouts/Polls/Polls.jsx
+++ src/components/layouts/Polls/Polls.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../layouts.css'
+import PhotoGroup from '../../common/PhotoGroup'
 
 export default function Polls() {
   return (
@@ -7,8 +8,12 @@ export default function Polls() {
         <h1>Polls</h1>
         <p>Who is the best actor in Marvel Cinematic Universe?</p>
         <input type="radio" /><label>Criss Hemsworth</label>
+        <PhotoGroup/>
         <br />
         <input type="radio" /><label>Robert Downey Junior</label>
+        <PhotoGroup/>
+        <br />
+
     </div>
   )
 }
