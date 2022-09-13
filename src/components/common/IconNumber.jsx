80ed9711d12386a7ce137a52440855af95a5bd5d
Jehan Veracierta <jehanzao@gmail.com>
2022-09-12 20:09:52 -0400

    IconNumber Number ready

--- src/components/common/IconNumber.jsx
+++ src/components/common/IconNumber.jsx
@@ -6,7 +6,7 @@ export default function IconNumber(props) {
   return (
     <div className='d-flex'>
         <IconButton active={props.active} iconActive={props.iconActive} iconInactive={props.iconInactive} color={props.color}/>
-        <p className='NumberAfterIcon'>{props.number}</p>
+        <p className='NumberAfterIcon'>{props.number >= 1000 ? (parseInt(props.number / 1000)) + 'k' : props.number}</p>
     </div>
   )
 }
